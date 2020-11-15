a4973e2ea79dd39d8f8d66b276fe059b37f5bd3e
Garrett Parker <garrett.parker@garrettspage.com>
2020-11-15 18:36:13 +0000

    Name Change

---
+++ server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var nStatic = require('node-static');
+var fileServer = new nStatic.Server('./PUBLIC');
+
+http.createServer(function (req, res) {
+	console.log(req);
+	console.log(res);
+	fileServer.serve(req, res);
+
+}).listen(8080);
